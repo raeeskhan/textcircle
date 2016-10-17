3a1391238611e5edc4b65288de46b74c9096c4fc
raeeskhan <username@users.noreply.github.com>
2016-10-17 22:29:39 +0500

    Revert "Week-3 completion"
    This reverts commit 6ba72c5f0a33e64b02c56e726969aec74fed6340.

--- server/main.js
+++ server/main.js
@@ -9,16 +9,3 @@ Meteor.startup(() => {
   //  Session.set("docid",id);
   }
 });
-
-Meteor.publish("documents", function(){
-	return Documents.find({
-		$or:[
-		{isPrivate: false},
-		{owner:this.userId}
-		]
-	});
-})
-
-Meteor.publish("editingUsers", function(){
-	return EditingUsers.find({});
-})
